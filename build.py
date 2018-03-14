4fde56e458341ee61e425c3e9f4a45c6f5fd8f40
Franck Pommereau <franck.pommereau@gmail.com>
2018-03-14 12:47:13 +0100

    added README and COPYING

--- build.py
+++ build.py
@@ -1,14 +1,28 @@
-import sys
+import argparse, os.path
 from distutils.core import setup
 from Cython.Build import cythonize
 from distutils.extension import Extension
 
+parser = argparse.ArgumentParser("build.py")
+parser.add_argument("-I", metavar="PATH", type=str,
+                    default=os.path.expanduser("~/.local/include"),
+                    help="libDDD source dir containing 'ddd/DDD.h' "
+                    "(default '$HOME/.local/include')")
+parser.add_argument("-L", metavar="PATH", type=str,
+                    default=os.path.expanduser("~/.local/lib"),
+                    help="libDDD binary dir (default '$HOME/.local/lib')")
+
+args = parser.parse_args()
+
 extensions = [Extension("ddd",
                         ["ddd.pyx"],
-                        include_dirs = ["."],
+                        include_dirs = [args.I],
                         libraries = ["DDD"],
-                        library_dirs = ["/home/franck/.local/lib"],
+                        library_dirs = [args.L],
                         language="c++")]
 
-sys.argv[1:] = ["build_ext", "--inplace"]
-setup(ext_modules = cythonize(extensions))
+setup(ext_modules=cythonize(extensions),
+      script_args=["build_ext", "--inplace"])
+
+import ddd
+print("built ddd module in %r" % os.path.relpath(ddd.__file__))
