adea5ff438af7a6954d0bd722b1d7b6febaead64
Franck Pommereau <franck.pommereau@gmail.com>
2022-07-11 15:08:01 +0200

    attempt to fix installation

--- setup.py
+++ setup.py
@@ -55,8 +55,6 @@ if not DDDPATH :
             self.copy_tree(f"{BUILD}/include", f"{self.install_base}/include")
             self.copy_tree(f"{BUILD}/lib", f"{self.install_base}/lib")
             self.mkpath(self.install_lib)
-            self.copy_file("dddwrap.h", self.install_lib)
-            self.copy_file("ddd.pxd", self.install_lib)
             super().run()
     setup(cmdclass={"install" : install})
 
@@ -76,6 +74,13 @@ print(f"### using '{DDDPATH}' ###")
 long_description = Path("README.md").read_text(encoding="utf-8")
 description = (long_description.splitlines())[0]
 
+class install (_install) :
+    def run (self) :
+        self.mkpath(self.install_lib)
+        self.copy_file("dddwrap.h", self.install_lib)
+        self.copy_file("ddd.pxd", self.install_lib)
+        super().run()
+
 setup(name="pyddd",
       description=description,
       long_description=long_description,
@@ -88,6 +93,7 @@ setup(name="pyddd",
                    "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
                    "Programming Language :: Python :: 3",
                    "Operating System :: OS Independent"],
+      cmdclass={"install" : install},
       ext_modules=cythonize([Extension("ddd",
                                        ["ddd.pyx"],
                                        language="c++",
