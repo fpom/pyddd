b15feffa6344def9f1b0d18081fff558e3640bde
Franck Pommereau <franck.pommereau@gmail.com>
2024-09-28 17:27:57 +0200

    added compile option

--- setup.py
+++ setup.py
@@ -10,59 +10,70 @@ import tarfile, os, ctypes, ctypes.util
 ## install precompiled libDDD
 ##
 
-BUILD  = "build"
+BUILD = "build"
 DDDTGZ = "libDDD.tar.gz"
 DDDINC = f"{BUILD}/include"
 
 Path(BUILD).mkdir(exist_ok=True)
-with tarfile.open(DDDTGZ) as tar :
+with tarfile.open(DDDTGZ) as tar:
     tar.extractall(BUILD)
 
 ##
 ## detect libDDD.so
 ##
 
-def which_ddd () :
+
+def which_ddd():
     so_name = ctypes.util.find_library("DDD")
-    if not so_name :
+    if not so_name:
         return None
     found = []
-    class dl_phdr_info (ctypes.Structure) :
-      _fields_ = [('padding0', ctypes.c_void_p),
-                  ('dlpi_name', ctypes. c_char_p)]
-    callback_t = ctypes.CFUNCTYPE(ctypes.c_int,
-                                  ctypes.POINTER(dl_phdr_info),
-                                  ctypes.POINTER(ctypes.c_size_t),
-                                  ctypes.c_char_p)
+
+    class dl_phdr_info(ctypes.Structure):
+        _fields_ = [("padding0", ctypes.c_void_p), ("dlpi_name", ctypes.c_char_p)]
+
+    callback_t = ctypes.CFUNCTYPE(
+        ctypes.c_int,
+        ctypes.POINTER(dl_phdr_info),
+        ctypes.POINTER(ctypes.c_size_t),
+        ctypes.c_char_p,
+    )
     dl_iterate_phdr = ctypes.CDLL("libc.so.6").dl_iterate_phdr
     dl_iterate_phdr.argtypes = [callback_t, ctypes.c_char_p]
     dl_iterate_phdr.restype = ctypes.c_int
+
     def callback(info, size, data):
         if data in info.contents.dlpi_name:
             found.append(info.contents.dlpi_name)
         return 0
+
     _ = ctypes.CDLL(so_name)
     dl_iterate_phdr(callback_t(callback), os.fsencode(so_name))
-    if found :
+    if found:
         return found[0].decode()
-    else :
+    else:
         return None
 
+
 DDDPATH = which_ddd()
-if not DDDPATH :
-    class install (_install) :
-        def run (self) :
+if not DDDPATH:
+
+    class install(_install):
+        def run(self):
             self.copy_tree(f"{BUILD}/include", f"{self.install_base}/include")
             self.copy_tree(f"{BUILD}/lib", f"{self.install_base}/lib")
             self.mkpath(self.install_lib)
             super().run()
-    setup(cmdclass={"install" : install})
+
+    setup(cmdclass={"install": install})
 
 DDDPATH = which_ddd()
-if not DDDPATH :
+if not DDDPATH:
     DDDPATH = f"{BUILD}/lib/libDDD.so"
-    print(f"### libDDD not found, using precompiled {DDDPATH}\n"
-          f"### this will not work until libDDD is properly installed")
+    print(
+        f"### libDDD not found, using precompiled {DDDPATH}\n"
+        f"### this will not work until libDDD is properly installed"
+    )
 
 DDDLIB = str(Path(DDDPATH).parent)
 print(f"### using '{DDDPATH}' ###")
@@ -74,26 +85,34 @@ print(f"### using '{DDDPATH}' ###")
 long_description = Path("README.md").read_text(encoding="utf-8")
 description = (long_description.splitlines())[0]
 
-setup(name="pyddd",
-      description=description,
-      long_description=long_description,
-      url="https://github.com/fpom/pyddd",
-      author="Franck Pommereau",
-      author_email="franck.pommereau@univ-evry.fr",
-      classifiers=["Development Status :: 4 - Beta",
-                   "Intended Audience :: Developers",
-                   "Topic :: Scientific/Engineering",
-                   "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-                   "Programming Language :: Python :: 3",
-                   "Operating System :: OS Independent"],
-      ext_modules=cythonize([Extension("ddd",
-                                       ["ddd.pyx"],
-                                       language="c++",
-                                       include_dirs = [DDDINC],
-                                       libraries = ["DDD"],
-                                       library_dirs = [DDDLIB],
-                                       extra_compile_args=["-std=c++11"],
-                                       extra_link_args=["-Wl,--no-as-needed"],
-      )],
-                            language_level=3),
+setup(
+    name="pyddd",
+    description=description,
+    long_description=long_description,
+    url="https://github.com/fpom/pyddd",
+    author="Franck Pommereau",
+    author_email="franck.pommereau@univ-evry.fr",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    ext_modules=cythonize(
+        [
+            Extension(
+                "ddd",
+                ["ddd.pyx"],
+                language="c++",
+                include_dirs=[DDDINC],
+                libraries=["DDD"],
+                library_dirs=[DDDLIB],
+                extra_compile_args=["-fno-lto", "-std=c++11"],
+                extra_link_args=["-Wl,--no-as-needed"],
+            )
+        ],
+        language_level=3,
+    ),
 )
